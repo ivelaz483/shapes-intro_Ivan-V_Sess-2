4490efcc390038ce10807a0b02eb0ccceff0159d
ivelaz483 <89948666+ivelaz483@users.noreply.github.com>
2022-02-01 18:15:50 -0700

    Add files via upload

---
+++ arc.js
@@ -0,0 +1,89 @@
+/*window.onload = function(){
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+    context.lineWidth = "3";
+    context.arc(300,125,25,0*Math.PI,2*Math.PI)
+    /*context.arc(canvas.width/2, canvas.height / 2 + 40, 80, 1.1 * Math.PI, 
+        1.9 * Math.PI, false);
+        context.stroke();
+    }*/
+
+    /*var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+    context.lineWidth = "3";
+    context.arc(200,25,15,1*Math.PI,1.5*Math.PI)
+    context.stroke();
+
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+    context.lineWidth = "3";
+    context.arc(400,25,15,1*Math.PI,1.5*Math.PI)
+    context.stroke();*/
+
+    //Object1
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+    context.lineWidth = "3";
+    context.arc(200,50,15,0*Math.PI,2*Math.PI)
+    context.stroke();
+
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+    context.lineWidth = "3";
+    context.arc(400,50,15,0*Math.PI,2*Math.PI)
+    context.stroke();
+
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+    context.lineWidth = "3";
+    context.arc(300,100,15,0*Math.PI,2*Math.PI)
+    context.stroke();
+
+    var canvas = document.getElementById("myCanvas");
+    var context = canvas.getContext("2d");
+    context.lineWidth = "3";
+    context.arc(300,135,75,0*Math.PI,1*Math.PI)
+    context.stroke();
+
+    //Object 2
+var canvas = document.getElementById("myCanvas2");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "orange";
+ctx.fillRect(0, 0, 600, 75);
+
+    var canvas = document.getElementById("myCanvas2");
+    var cts = canvas.getContext("2d");
+    cts.lineWidth = "3";
+    cts.arc(300,125,15,0*Math.PI,2*Math.PI)
+    cts.fillStyle = "#000066";
+    cts.fill(); 
+
+    var canvas = document.getElementById("myCanvas2");
+    var ctd = canvas.getContext("2d");
+    ctd.lineWidth = "3";
+    ctd.arc(300,125,25,0*Math.PI,2*Math.PI)
+    cts.strokeStyle = "#000066";
+    cts.stroke();
+
+    var canvas = document.getElementById("myCanvas2");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 175, 600, 75);
+
+
+    //Object 3
+    var canvas = document.getElementById("myCanvas3");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#0066cc";
+    ctx.fillRect(0, 0, 600, 275);
+
+    var canvas = document.getElementById("myCanvas3");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(0, 75, 600, 75);
+    
+        var canvas = document.getElementById("myCanvas3");
+        var ctx = canvas.getContext("2d");
+        ctx.rotate(0 * Math.PI / 180);
+        ctx.fillStyle = "yellow";
+        ctx.fillRect(150, 0, 100, 600);
